652a11cf552b8e1c4fffbed17a0fdd0b6afdadf5
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2022-10-18 11:24:34 +0200

    refactor

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,18 +7,18 @@ export default function Cart() {
   const [isOpen, setIsOpen] = useState(false)
   return isOpen
     ? (
-    <div className='lmj-cart'>
-      <button className='lmj-cart-toggle-button' onClick={() => setIsOpen(false)}>Fermer</button>
-      <h2>Panier</h2>
-      <div>Monstera : {monsteraPrice}€</div>
-      <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
-      <h3>Total : {monsteraPrice * cart}€</h3>
-      <button onClick={() => updateCart(0)}>Vider le panier</button>
-    </div>
+      <div className='lmj-cart'>
+        <button className='lmj-cart-toggle-button' onClick={() => setIsOpen(false)}>Fermer</button>
+        <h2>Panier</h2>
+        <div>Monstera : {monsteraPrice}€</div>
+        <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+        <h3>Total : {monsteraPrice * cart}€</h3>
+        <button onClick={() => updateCart(0)}>Vider le panier</button>
+      </div>
       )
     : (
-    <div className='lmj-cart-closed'>
-      <button className='lmj-cart-toggle-button' onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
-    </div>
+      <div className='lmj-cart-closed'>
+        <button className='lmj-cart-toggle-button' onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
+      </div>
       )
 }
